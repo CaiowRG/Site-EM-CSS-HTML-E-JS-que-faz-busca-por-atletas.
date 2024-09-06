550ee3f1e05c1f31fa06a00359cb8a6e3b55dd4f
CaiowRG <caioribgon@gmail.com>
2024-09-06 18:16:51 -0300

    Add files via upload
    Fase modelo do site

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo:"Rayssa Leal",
+        descrição: "Rayssa Leal, a Fadinha, é uma skatista brasileira",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate sk8 fada fadinha"
+    },
+    {
+        titulo:"Beatriz Souza",
+        descrição: "Beatriz Souza é uma judoca brasileira",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoca judoka"
+
+    },
+    {
+        titulo:"Rebeca Andrade",
+        descrição: "Rebeca andrade é uma ginásta brasileira",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginasta ouro ginastica"  
+    }
+];
